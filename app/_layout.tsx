bc97be9a8ec95b5e141b8fb8ebbb6b8f06837281
adnanmusil <adnan.museel123@gmail.com>
2025-08-17 21:44:26 +0500

    updated work

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,14 @@ import { Stack } from "expo-router";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
-  </Stack>;
+  return (
+    <>
+    {/* <StatusBar hidden={true} /> */}
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>
+    </>
+
+  );
 }
